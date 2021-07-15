a741a6c28219de2fe805c3c8bffdf80285d3e351
Ryan Walker <ryandanielwalker@gmail.com>
2021-07-15 10:21:19 -0700

    Add tests for reducer and actions
    Co-Authored-By: Marney Mallory<marney.mallory@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './reducers';
+import middlewareExample from './middleware/middleware-example';
+
+const store = createStore(reducer, applyMiddleware(middlewareExample));
 
 ReactDOM.render(
   <React.StrictMode>
